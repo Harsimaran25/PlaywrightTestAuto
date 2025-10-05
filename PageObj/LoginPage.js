fdb1c9caf9c12ed697a71de224b9bc936fd1d21b
Hars <harsimaran.singh25@outlook.com>
2025-10-05 12:37:31 +1100

    addedpage

---
+++ PageObj/LoginPage.js
@@ -0,0 +1,18 @@
+// this is Page for login page
+
+class LoginPage
+
+{
+
+
+constructor(page)
+
+{
+    this.page=page;
+    this.userName= page.getByPlaceholder('email@example.com');
+    this.passWord=
+    this.loginBtn=
+
+}
+
+}
