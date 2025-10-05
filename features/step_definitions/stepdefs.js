7014f1623f8254736edfb2c4936d01ff6b5c91db
Hars <harsimaran.singh25@outlook.com>
2025-10-05 20:14:21 +1100

    formatted

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -4,100 +4,117 @@
   the scenarios in Gherkin format that are 
  Given, When, and then format, and the Step Definition file contains the actual code or implementation of test cases.*/
 
-const assert = require('assert');
-const { Given, When, Then } = require('@cucumber/cucumber');
+const assert = require("assert");
+const { Given, When, Then } = require("@cucumber/cucumber");
 const { Page } = require("playwright");
 const { chromium, expect } = require("@playwright/test");
 
-let ordidraw,ordid,productname; //global scope here 
-Given('user logs to site with {string} and {string}',{ timeout: 60000 }, async function (username, password) {
-           // Write code here that turns the phrase above into concrete actions
-// but the page or browser this stepdefinition  does not know  so we 
-// browser = await chromium.launch({ headless: false });
+let ordidraw, ordid, productname; //global scope here
+Given(
+  "user logs to site with {string} and {string}",
+  { timeout: 60000 },
+  async function (username, password) {
+    // Write code here that turns the phrase above into concrete actions
+    // but the page or browser this stepdefinition  does not know  so we
+    // browser = await chromium.launch({ headless: false });
 
-// const context = await browser.newContext();
+    // const context = await browser.newContext();
 
-// page = await context.newPage();
+    // page = await context.newPage();
     // await page.goto('https://rahulshettyacademy.com/client/#/auth/login');
     // await page.waitForLoadState('networkidle');
 
-      await this.page.goto('https://rahulshettyacademy.com/client/#/auth/login', {
-        waitUntil: 'networkidle'
+    await this.page.goto("https://rahulshettyacademy.com/client/#/auth/login", {
+      waitUntil: "networkidle",
     });
-// Wait and fill credentials
+    // Wait and fill credentials
     await this.page.waitForSelector('[placeholder="email@example.com"]');
-    await this.page.getByPlaceholder('email@example.com').fill(username);
-    await this.page.getByPlaceholder('enter your passsword').fill(password);
-    await this.page.getByRole('button', { name: 'Login' }).click();
-   
-//just used to let this load 
-console.log('in given');
+    await this.page.getByPlaceholder("email@example.com").fill(username);
+    await this.page.getByPlaceholder("enter your passsword").fill(password);
+    await this.page.getByRole("button", { name: "Login" }).click();
 
-          
-         });
+    //just used to let this load
+    console.log("in given");
+  }
+);
 
+When("user adds product {string} to cart", async function (product) {
+  // Write code here that turns the phrase above into concrete actions
+  //get product and add to cart using filter
+  await this.page.locator(".card-body b").first().textContent();
+  console.log("in when");
+  productname = product;
+  await this.page
+    .locator(".card-body")
+    .filter({ hasText: product })
+    .getByRole("button", { name: "Add to Cart" })
+    .click();
+});
 
-When('user adds product {string} to cart', async function (product) {
-           // Write code here that turns the phrase above into concrete actions
-        //get product and add to cart using filter
-        await this.page.locator('.card-body b').first().textContent();
-        console.log('in when')
-        productname=product;
-        await this.page.locator('.card-body').filter({hasText:product}).getByRole('button',{name:'Add to Cart'}).click();
-         });
+Then("verify product is added to the cart", async function () {
+  // Write code here that turns the phrase above into concrete actions
+  console.log("in Then");
+  await this.page
+    .getByRole("listitem")
+    .getByRole("button", { name: "Cart" })
+    .click();
 
- Then('verify product is added to the cart', async function () {
-           // Write code here that turns the phrase above into concrete actions
-    console.log('in Then')
-           await this.page.getByRole('listitem').getByRole('button',{name:'Cart'}).click();
+  await this.page.locator("div.cartSection").first().waitFor(); // just to make sure all is loaded and synced
 
-        await this.page.locator('div.cartSection').first().waitFor(); // just to make sure all is loaded and synced
+  await expect(this.page.getByText(productname)).toBeVisible();
+  await this.page.getByRole("button", { name: "Checkout" }).click();
 
-        await expect(this.page.getByText(productname)).toBeVisible(); 
-        await this.page.getByRole('button',{name:'Checkout'}).click();
+  await this.page.waitForLoadState("networkidle");
+  await this.page.getByPlaceholder("Select Country").pressSequentially("Ind");
+  await this.page.getByRole("Button", { name: /\s*India$/ }).click();
 
-    await this.page.waitForLoadState('networkidle');
-    await this.page.getByPlaceholder('Select Country').pressSequentially('Ind');
-    await this.page.getByRole('Button',{name: /\s*India$/}).click();
+  await this.page.getByText("PLACE ORDER").click();
 
-    await this.page.getByText('PLACE ORDER').click();
+  //await expect(page.getByRole('heading',{name:'Thankyou for the order.'})).toBeVisible();  OR
 
-    //await expect(page.getByRole('heading',{name:'Thankyou for the order.'})).toBeVisible();  OR
+  await expect(this.page.getByText("Thankyou for the order.")).toBeVisible();
+  ordidraw = await this.page.locator("label.ng-star-inserted").textContent();
+  console.log(ordidraw);
+  ordid = ordidraw.replace(/^\s*\|\s*|\s*\|\s*$/g, "").trim();
+  console.log(ordid);
+});
 
-    await expect(this.page.getByText('Thankyou for the order.')).toBeVisible();
-    ordidraw=await this.page.locator('label.ng-star-inserted').textContent();
-    console.log(ordidraw);
-     ordid= ordidraw.replace(/^\s*\|\s*|\s*\|\s*$/g, '').trim();
-    console.log(ordid);
-            
-         });
+Then("product is present in orderhistory page", async function () {
+  // Write code here that turns the phrase above into concrete actions
 
+  console.log("in then 73 line no");
+  await this.page.locator('label[routerlink*="myorders"]').click();
 
-         Then('product is present in orderhistory page', async function () {
-           // Write code here that turns the phrase above into concrete actions
+  await expect(
+    this.page.getByRole("heading", { name: "Your Orders" })
+  ).toBeVisible();
+  //const p= await page.getByRole('rowheader')
 
-           console.log('in then 73 line no')
-           await this.page.locator('label[routerlink*="myorders"]').click();
-
-await expect(this.page.getByRole('heading',{name:'Your Orders'})).toBeVisible();
-//const p= await page.getByRole('rowheader')
-
-const ordrows=await this.page.locator('tr.ng-star-inserted').count();
-console.log(ordrows);
-for(let i=0;i<ordrows;i++)
-    {
-      let k= await this.page.locator('tr.ng-star-inserted').nth(i).locator('th').textContent();
-      if(ordidraw.includes(k)){
-       await this.page.locator('tr.ng-star-inserted').nth(i).locator('button').first().click();
-   break;
-      }
-
+  const ordrows = await this.page.locator("tr.ng-star-inserted").count();
+  console.log(ordrows);
+  for (let i = 0; i < ordrows; i++) {
+    let k = await this.page
+      .locator("tr.ng-star-inserted")
+      .nth(i)
+      .locator("th")
+      .textContent();
+    if (ordidraw.includes(k)) {
+      await this.page
+        .locator("tr.ng-star-inserted")
+        .nth(i)
+        .locator("button")
+        .first()
+        .click();
+      break;
     }
+  }
 
-    await expect(this.page.locator('.email-title')).toContainText(' order summary ');
-const orderdetailpage= await this.page.locator('.col-text').textContent();
-expect(ordidraw.includes(orderdetailpage)).toBeTruthy();
-console.log('finished test')
+  await expect(this.page.locator(".email-title")).toContainText(
+    " order summary "
+  );
+  const orderdetailpage = await this.page.locator(".col-text").textContent();
+  expect(ordidraw.includes(orderdetailpage)).toBeTruthy();
+  console.log("finished test");
 
-//or use command in CLI npx cucumber-js --exit
-           });
+  //or use command in CLI npx cucumber-js --exit
+});
--- features/step_definitions/support/hooks.js
+++ features/step_definitions/support/hooks.js
@@ -1,43 +1,42 @@
 /**Hooks are used for setup and teardown the environment before and after each scenario */
 
-const { Before,After,AfterStep,Status} = require('@cucumber/cucumber');
+const { Before, After, AfterStep, Status } = require("@cucumber/cucumber");
 const { Page } = require("playwright");
 const { chromium } = require("@playwright/test");
 
 /**By storing browser and page on this, you make them accessible in step definitions */
 
-//the path for the hooks is set in cucumber.json file 
-Before( async function () {
+//the path for the hooks is set in cucumber.json file
+Before(async function () {
+  console.log("Launching browser before scenario...");
 
- console.log('Launching browser before scenario...');
+  // Create and store browser/page in the Cucumber World (`this`)
+  this.browser = await chromium.launch({ headless: false });
 
- // Create and store browser/page in the Cucumber World (`this`)
-this.browser = await chromium.launch({ headless: false });
+  const context = await this.browser.newContext();
 
-const context = await this.browser.newContext();
+  this.page = await context.newPage();
 
-this.page = await context.newPage();
-
-//return page;
-
+  //return page;
 });
 
-
-After( async function () {
-  
-    console.log('exiting')   //Checks if this.browser exists:Prevents errors in case the browser wasn't launched for some reason
-   if (this.browser) {
+After(async function () {
+  console.log("exiting"); //Checks if this.browser exists:Prevents errors in case the browser wasn't launched for some reason
+  if (this.browser) {
     await this.browser.close();
-  } 
+  }
 });
 /**What is result?
 The result object contains the status and outcome of the step that just ran. 
 This is provided by Cucumber.js (not Playwright) and is passed to the AfterStep hook automatically. 
 key properties of result: status , duration and exception*/
 
-AfterStep( async function ({result}) {
+AfterStep(async function ({ result }) {
   // This hook will be executed after all steps, and take a screenshot on step failure
   if (result.status === Status.FAILED) {
-   const screenshot = await this.page.screenshot({ path: `CucumberScreenshots/${Date.now()}-failed.png`, fullPage: true });
+    const screenshot = await this.page.screenshot({
+      path: `CucumberScreenshots/${Date.now()}-failed.png`,
+      fullPage: true,
+    });
   }
 });
