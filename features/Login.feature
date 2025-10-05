09f7159b2c49a08159f25bbef51b7db81ebf924f
Hars <harsimaran.singh25@outlook.com>
2025-10-05 15:40:28 +1100

    added stepdef

---
+++ features/Login.feature
@@ -0,0 +1,8 @@
+# This is a comment explaining the feature below
+Feature: Login to Rahulshetty
+
+    Scenario: Login using getbylocator
+        Given user logs to site with "userName" and "passWord"
+        When user adds product "ZARA COAT 3" to cart
+        Then verify product is added to the cart
+        And product is present in orderhistory page
---
+++ features/step_definitions/stepdefs.js
@@ -0,0 +1,9 @@
+// this is step definistion file which will have the code
+
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('user logs to site with {username} and {password}', function (string, string2) {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
