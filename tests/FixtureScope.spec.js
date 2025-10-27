a54af03dfcbf6df61ff2b114e34d3362f826b26f
Hars <harsimaran.singh25@outlook.com>
2025-10-27 19:41:33 +1100

    addednewtests

--- tests/FixtureScope.spec.js
+++ tests/FixtureScope.spec.js
@@ -36,3 +36,16 @@ customFixtureScope("test 3 with worker scope", async ({ counterFixture }) => {
  * while browser handles the browser instance. Context and request fixtures. Context manages isolated sessions,
  * and requests is great for API testing without a browser. Advance fixtures, trace captures detailed test traces,
  *  and artifacts handle screenshots, videos, and more. */
+
+/**What is a page object model?
+ Think of your web application as a large library. Each webpage is like a different book in this library. 
+Instead of searching for specific information yourself, imagine having a librarian who knows exactly where everything is. 
+In this analogy, the librarian represents the page object model. The page object model, or POM, acts like a librarian for
+ your web application. Each webpage is presented by a specific class in your code. Just like a librarian knows where each 
+ book is located, the POM knows all the elements on a page, and how to interact with them. For example, the login page
+  might be book one, the dashboard might be book two, and so on. This organization helps streamline your tests. Just 
+  like a librarian makes finding books quicker and easier, the POM makes writing and maintaining tests more efficient and 
+  consistent. With POM, you don't need to know every detail about a webpage. The POM takes care of it for you, simplifying
+   the process. Here's a real world example of using POM, logging into a website. Without POM, you'll need to manually 
+   find the login fields and enter your credentials in every test. With POM, it's like telling the librarian to log you in. 
+   The POM knows exactly what to do and handles it for you, saving time and effort. */
