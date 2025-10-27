f7c10f130660da8a0cb4268d8d03a2308d76f0b7
Hars <harsimaran.singh25@outlook.com>
2025-10-27 19:33:31 +1100

    addednewtests

---
+++ tests/FixtureScope.spec.js
@@ -0,0 +1,30 @@
+//linked in design patterns lesson to   fixture scope and isolation
+/* the difference between two fixer scopes in Playwright, task scope and worker scope. 
+These scopes control how and when fixtures are created, which impacts how state is managed across your test*/
+
+import { expect, test as base } from "@playwright/test";
+
+let counter = 0;
+const customFixtureScope = base.extend({
+  counterFixture: [
+    async ({}, use) => {
+      counter++;
+      await use(counter);
+    },
+    { scope: "worker" },
+  ],
+});
+
+//test here
+
+customFixtureScope("test 1 with worker scope", async ({ counterFixture }) => {
+  console.log(`Test 1 counter ${counter}`);
+});
+
+customFixtureScope("test 2 with worker scope", async ({ counterFixture }) => {
+  console.log(`Test 1 counter ${counter}`);
+});
+
+customFixtureScope("test 3 with worker scope", async ({ counterFixture }) => {
+  console.log(`Test 1 counter ${counter}`);
+});
